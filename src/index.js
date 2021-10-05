eff5b8d5e4ac389217df789b66d272dcfd8b592a
tgpretender <jtpretender@yahoo.com>
2021-10-05 18:37:44 -0500

    beginning react setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {
+	Header,
+	Footer
+} from './components';
+
 const App = () => {
-	return 'Hello';
+	return <div>
+		<Header />
+		<main>
+			Hello
+		</main>
+		<Footer />
+	</div>
 };
 
 ReactDOM.render(<App />, document.getElementById('app'));
